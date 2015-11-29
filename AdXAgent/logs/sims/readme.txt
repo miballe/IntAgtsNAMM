220ea62b13bb728865d4221dce16c84e0edd5086
miballeuk <mabm1e15@j20.ecslabs.soton.ac.uk>
2015-11-29 20:07:43 +0000

    Project refactory 1

---
+++ AdXAgent/logs/sims/readme.txt
@@ -0,0 +1,1 @@
+SIMS logs folder
--- AdXAgent/runAgent.sh
+++ AdXAgent/runAgent.sh
@@ -8,4 +8,4 @@ TACAA_HOME=`pwd`
 echo $TACAA_HOME
 echo $CLASSPATH
 
-java -cp "lib/*" tau.tac.adx.agentware.Main -config config/aw-1.conf
+java -cp "lib/*" tau.tac.adx.agentware.Main -config config/agent1.conf
