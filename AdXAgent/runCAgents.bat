d8269ed2cac57fa3076f7ff1412a299b5956eccf
Miguel Ballesteros <miballeuk@outlook.com>
2015-12-02 16:21:17 +0000

    Added batch to run several agents and deleted unnecessary files

---
+++ AdXAgent/runCAgents.bat
@@ -0,0 +1,7 @@
+start java -cp "PrevAgent_jar/*" tau.tac.adx.agentware.Main -config PrevAgent_jar/comp01.conf
+start java -cp "PrevAgent_jar/*" tau.tac.adx.agentware.Main -config PrevAgent_jar/comp02.conf
+start java -cp "PrevAgent_jar/*" tau.tac.adx.agentware.Main -config PrevAgent_jar/comp03.conf
+start java -cp "PrevAgent_jar/*" tau.tac.adx.agentware.Main -config PrevAgent_jar/comp04.conf
+start java -cp "PrevAgent_jar/*" tau.tac.adx.agentware.Main -config PrevAgent_jar/comp05.conf
+start java -cp "PrevAgent_jar/*" tau.tac.adx.agentware.Main -config PrevAgent_jar/comp06.conf
+start java -cp "PrevAgent_jar/*" tau.tac.adx.agentware.Main -config PrevAgent_jar/comp07.conf
